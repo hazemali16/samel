ad0af7f2cf200df7c8fc93f613a35aad8aa845ae
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-08-15 11:41:22 +0300

    Update Products.jsx

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -13,14 +13,27 @@ const Products = () => {
             <Image src={product1} alt="product1" className="rounded-t-lg h-80 object-cover" />
             <div className="p-5 pb-10">
             <h3 className="text-2xl mb-5 text-[var(--main-color)] font-bold">مفك صامل الذكي</h3>
-            <p className="leading-relaxed h-28">رفيقك في كل المواقف، تصميم عملي يريحك من تعب البحث عن المفكات الضايعة، تفك وتركب فيه مع طقم متنوع من 51 رأس، يناسب كل أنواع المسامير.</p>
+            <p className="leading-relaxed min-h-24">رفيقك في كل المواقف، تصميم عملي يريحك من تعب البحث عن المفكات الضايعة، تفك وتركب فيه مع طقم متنوع من 51 رأس، يناسب كل أنواع المسامير.</p>
+            <ul className="list-disc p-5">
+              <li>عملي وسهل</li>
+              <li className="my-3">بجودة عالية ومُبتكر</li>
+              <li>51 رأس متغيير</li>
+              <li className="my-3">كشاف للإضاءة</li>
+              <li>بطارية قوية</li>
+            </ul>
             </div>
         </div>
         <div className="shadow-lg rounded-lg">
             <Image src={product2} alt="product2" className="rounded-t-lg h-80 object-cover" />
             <div className="p-5 pb-10">
             <h3 className="text-2xl mb-5 text-[var(--main-color)] font-bold">لاصق النانو السحري</h3>
-            <p className="leading-relaxed h-28">ابتكار ولا أروع، يلصق لك أي شيء على أي سطح بدون أثر، مع إمكانية غسله وتكرار استخدامه.</p>
+            <p className="leading-relaxed h-24">ابتكار ولا أروع، يلصق لك أي شيء على أي سطح بدون أثر، مع إمكانية غسله وتكرار استخدامه.</p>
+            <ul className="list-disc p-5">
+              <li>ما يترك أثر</li>
+              <li className="my-3">ثابت وقوي بوجهين</li>
+              <li className="mb-3">شفاف وعصري للإستخدامات متعددة</li>
+              <li>مقاوم للماء</li>
+            </ul>
 
             </div>
         </div>
@@ -28,10 +41,7 @@ const Products = () => {
     <div className="mt-20 text-center text-2xl text-[var(--main-color)]">
     <p className="mt-5 text-[var(--main-color)] font-bold mb-2">سعرنا الحصري لك: 170 ريال فقط!</p>
 
-
-
     لا تفوت الفرصة، الكمية محدودة والسعر ما يتكرر!
-
 
     </div>
   </div>
